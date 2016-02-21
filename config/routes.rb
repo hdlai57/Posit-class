67dbcf822d0bf41f9d003f046f50ed6d4ad10c53
hdlai57 <hdlai57@gmail.com>
2016-02-21 13:35:53 +0800

    copy from class demo, add Heroku gem

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,11 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+root 'posts#index'
+
+resources :posts do
+  resources :comments
+end
   # Example resource route with options:
   #   resources :products do
   #     member do
